ec0499267ec8af72ba06a19dded737965f8c5822
MauricioLópez <ingsmauriciolopez@gmail.com>
2026-05-14 20:57:20 -0500

    Condicion

--- prueba.py
+++ prueba.py
@@ -1,1 +1,13 @@
 print("Inicio de proyecto")
+# Definición de variables
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Hola", nombre)
+print("Tu edad es:", edad)
+
+# Condición
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
